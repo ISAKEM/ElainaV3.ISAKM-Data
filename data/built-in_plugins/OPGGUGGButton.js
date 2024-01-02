989d96317dbaac723ad23b740fccb3c65c0c80bd
unknown <Isak.Martensson@Hotmail.com>
2024-01-02 21:56:32 +0100

    Added OP.GG and U.GG buttons to check your ranked lobby

---
+++ data/built-in_plugins/OPGGUGGButton.js
@@ -0,0 +1,27 @@
+import utils from "../_utils.js"
+
+if (DataStore.get("Renzskin")) {
+    const addRenzskinButton = () => {
+        const button = document.createElement("lol-uikit-flat-button")
+        const position = "15px"
+        if (document.getElementById("renzskin-button")) {
+        }
+        else {
+            try {
+                button.id = "renzskin-button"
+                button.textContent = "R3nzSkin"
+                button.style.marginRight = position;
+                button.onclick = async () => {
+                    window.openPluginsFolder(`ISAKV1/data/assets/R3nzskin`)
+                }
+                let gameBar = document.querySelector(".lobby-header-content")
+                gameBar.insertBefore(button, gameBar.children[5])
+            } catch
+            {
+            }
+        }
+    }
+    window.addEventListener("load", () => {
+    utils.routineAddCallback(addRenzskinButton, ["rcp-fe-lol-parties"])
+    })
+}
